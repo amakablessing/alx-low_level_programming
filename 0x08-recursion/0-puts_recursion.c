5082ef293a35993c7f91d3e7fee632875e77382e
eze blessing <blessingchiaha@yahoo.com>
2022-09-28 18:36:07 -0700

    done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * a function that prints a string
+ *
+ * REturn: always 0.
+ * */
+
+
+int main(void)
+{
+  void _puts_recursion(char *s);
+      	
+        char _recursion[7] = string;
+        
+            _putchar("%s", _recursion);
+
+             return (0);
+}	     
