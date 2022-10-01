256bfcc9821557c37f3d0bc77d6634d0ef946753
eze blessing <blessingchiaha@yahoo.com>
2022-09-30 20:47:20 -0700

    accomplished

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,7 +11,7 @@
  * Return: returns (0)
  */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n",argc - 1);
 	return (0);
